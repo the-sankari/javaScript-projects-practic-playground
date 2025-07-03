529a9016fc565604a4be58acf9ce65b273c86afc
the-sankari <kajol.sd777@gmail.com>
2025-07-04 00:20:02 +0300

    Basic event handling

--- todo-app/main.js
+++ todo-app/main.js
@@ -4,3 +4,12 @@ import loadUniversalNavbar from "../js/universalNavbar.js";
 
 // Load the universal navbar (it will auto-detect the correct path)
 loadUniversalNavbar();
+
+const addTodoButton = document.querySelector("#add-todo");
+const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
+
+addTodoButton.addEventListener("click", () => {
+  const todoText = todoInput.value.trim();
+  console.log(`Todo added: ${todoText}`);
+});
